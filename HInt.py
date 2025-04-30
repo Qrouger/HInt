58dd19a41ecc155c82a8e624cc20131fd1258329
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-04-30 16:48:35 +0200

    Update HInt.py

--- HInt.py
+++ HInt.py
@@ -27,18 +27,8 @@ class Logger(object):
 sys.stdout = Logger(log_filename)
 sys.stderr = Logger(log_filename)
 
-
-def add_arguments(parser) :
-   # parser.add_argument("--use_mmseq", help = "Use MMseqs2 for feature generation (True/False)", required = False, default = True)
-   # parser.add_argument("--make_multimers", help = "Enable or disable multimer model generation (True/False) and analyse", required = False, default = True)
-   # parser.add_argument("--max_aa" , help = "Maximum number of amino acids that can be generated per cluster", required = False, default = 2500, type = int)
-   # parser.add_argument("--org" , help = "Organism of interest: arch, gram+, gram-, or euk for SignalP", required = False, default = "gram-", type = str)
-
 def main() :
     path_dict = define_path()
-    parser = argparse.ArgumentParser()
-    add_arguments(parser)
-    args = parser.parse_args()
     HInt_object = File_proteins(path_dict["Path_Uniprot_ID"])
     HInt_object.find_proteins_sequence() #and real name of proteins
     if len(HInt_object.already_pickle(path_dict["Path_Pickle_Feature"])) > 0 : #if new feature pickle is need
