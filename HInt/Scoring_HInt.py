958700ca78ce4ace41fb6ffcb3a007d7e8f3ac06
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-03-03 11:35:48 +0100

    Fix import statement for get_good_inter_pae

--- HInt/Scoring_HInt.py
+++ HInt/Scoring_HInt.py
@@ -8,7 +8,7 @@ import logging
 import multiprocessing
 from tqdm import tqdm
 from datetime import datetime
-import get_good_inter_pae
+import .get_good_inter_pae
 import gc
 import pandas as pd
 import json
