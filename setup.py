488204dbb4db64f2f1036e63c17c41aa071b0fc6
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-03-12 14:58:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
-        'alphapulldown==2.1.9',
+        'alphapulldown==2.1.8',
         'matplotlib',
         'nvidia-ml-py',
         'ihm',
