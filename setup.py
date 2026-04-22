3d55e24e7ae362ab1358655ccc483df83db2c715
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-04-22 13:17:39 +0200

    Bump to 0.5.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='HInt-ppi',
-    version='0.5.3.2',
+    version='0.5.4',
     description=(
         'A tool to find homologous interactions and speed up AlphaFold-based structural modeling.'
     ),
