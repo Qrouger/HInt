640a69c5287e6f967baae010ee76e5623aa57fb2
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-09-18 17:38:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
-        'AlphaPulldown @ git+https://github.com/Qrouger/AlphaPulldown.git#egg=alphapulldown',
         'matplotlib',
         'nvidia-ml-py',
         'ihm',
